a9f60d5205258d1fdf15e53bf1ff524e90f2c740
akashsangal09 <akashsangal09@gmail.com>
2022-11-29 22:46:23 +0530

    host name change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,31 +67,31 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  # config.action_mailer.default_url_options = { host: '127.0.0.1:25', port: 3000 }
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  # address:              'smtp.gmail.com',
-  # port:                 587,
-  # domain:               'example.com',
-  # user_name:            'akash.s@pixoatic.com',
-  # password:             'zliemogsccxnkzcu',
-  # authentication:       'plain',
-  # enable_starttls_auto: true
-  #  }
-
-
-  config.action_mailer.default_url_options = {host: '0.0.0.0:3000'}
-  config.action_mailer.default charset: 'utf-8'
-  config.action_mailer.delivery_method = 'smtp'
+  config.action_mailer.default_url_options = { host: 'useresdata.herokuapp.com', port: 3000 }
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  adress: $SMTP_SERVER,
-  port: $PORT,
-  from: $MAIL,
-
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            'akash.s@pixoatic.com',
+  password:             'zliemogsccxnkzcu',
+  authentication:       'plain',
   enable_starttls_auto: true
-  #authentication: 'login'
+   }
+
+
+  # config.action_mailer.default_url_options = {host: '0.0.0.0:3000'}
+  # config.action_mailer.default charset: 'utf-8'
+  # config.action_mailer.delivery_method = 'smtp'
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.smtp_settings = {
+  # adress: $SMTP_SERVER,
+  # port: $PORT,
+  # from: $MAIL,
+
+  # enable_starttls_auto: true
+  # #authentication: 'login'
 }
 
 
