25f4a9970c4e4f04b2dc7019b0a666ccca252ce9
akashsangal09 <akashsangal09@gmail.com>
2022-11-29 21:54:58 +0530

    smtp setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,18 +67,32 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { host: '127.0.0.1:25', port: 3000 }
-  config.action_mailer.perform_deliveries = false
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { host: '127.0.0.1:25', port: 3000 }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  # address:              'smtp.gmail.com',
+  # port:                 587,
+  # domain:               'example.com',
+  # user_name:            'akash.s@pixoatic.com',
+  # password:             'zliemogsccxnkzcu',
+  # authentication:       'plain',
+  # enable_starttls_auto: true
+  #  }
+
+
+  config.action_mailer.default_url_options = {host: '0.0.0.0:3000'}
+  config.action_mailer.default charset: 'utf-8'
+  config.action_mailer.delivery_method = 'smtp'
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'example.com',
-  user_name:            'akash.s@pixoatic.com',
-  password:             'zliemogsccxnkzcu',
-  authentication:       'plain',
+  adress: $SMTP_SERVER,
+  port: $PORT,
+  from: $MAIL,
+
   enable_starttls_auto: true
-   }
+  #authentication: 'login'
+}
 
 
 
