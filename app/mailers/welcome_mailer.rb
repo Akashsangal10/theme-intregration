03b92b01f0f0f7916b46a6e8ddde322498e9a0d8
akashsangal09 <akashsangal09@gmail.com>
2022-11-29 19:09:08 +0530

    mailer

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,8 +2,8 @@ class WelcomeMailer < ApplicationMailer
 
 	def contact_user(user)
 		@user = user
-		p "================"
+		
 		p "#{@user.inspect}"
-    	mail to: @user, subject: "Quill Informatics" 
+    	mail to: @user.email, subject: "Quill Informatics" 
 	end
 end
