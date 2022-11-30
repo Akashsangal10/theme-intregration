0484a8fb24630b60f2e52c6170821722197622dd
akashsangal09 <akashsangal09@gmail.com>
2022-11-30 10:10:34 +0530

    port add

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,9 +67,14 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { host: 'useresdata.herokuapp.com', port: 3000 }
-  config.action_mailer.perform_deliveries = true
+ 
+#     config.action_mailer.perform_deliveries = true
+# config.action_mailer.default_url_options = { :host => "localhost:3000" }
+# config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
@@ -77,24 +82,8 @@ Rails.application.configure do
   user_name:            'akash.s@pixoatic.com',
   password:             'zliemogsccxnkzcu',
   authentication:       'plain',
-  enable_starttls_auto: true
-   }
-
-
-  # config.action_mailer.default_url_options = {host: '0.0.0.0:3000'}
-  # config.action_mailer.default charset: 'utf-8'
-  # config.action_mailer.delivery_method = 'smtp'
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.smtp_settings = {
-  # adress: $SMTP_SERVER,
-  # port: $PORT,
-  # from: $MAIL,
-
-  # enable_starttls_auto: true
-  # #authentication: 'login'
-}
-
-
-
+  enable_starttls_auto: true,
+  open_timeout:         5000,
+  read_timeout:         5000 }
 
 end
