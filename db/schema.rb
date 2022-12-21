f02482e62cb9e2eec5a6f6da1386bcb0819f27ce
akashsangal09 <akashsangal09@gmail.com>
2022-12-21 13:36:43 +0530

    database name change

--- db/schema.rb
+++ db/schema.rb
@@ -76,6 +76,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_073548) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
